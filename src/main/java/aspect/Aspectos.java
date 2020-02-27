8b15fb1fa16d8c2477c496a75fa14d950761a71b
Matias <mdsmatias@hotmail.com>
2020-02-27 23:14:22 +0100

    afadsfa

--- src/main/java/aspect/Aspectos.java
+++ src/main/java/aspect/Aspectos.java
@@ -19,8 +19,7 @@ public class Aspectos {
         log.info("Aquí estoy antes de ejecutar el método");
         Object proceed = pjp.proceed();
         log.info("Aquí estoy después de ejecutar el método");
-
-
+        
 
 
         log.info("Cambiado desde la rama");
